ef1f6f318ee7cb67ee4a6a46f2b317069cce1825
hajer <hajer.zitouni@esprit.tn>
2020-12-10 13:54:14 +0100

    second

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListFilmComponent} from './list-film/list-film.component';
+import {FilmComponent} from './film/film.component';
+import {UpdatefilmComponent} from './updatefilm/updatefilm.component';
+import {DetailfilmComponent} from './detailfilm/detailfilm.component';
+import {ReserverComponent} from './reserver/reserver.component';
+import {RegistrationComponent} from './registration/registration.component';
+import {LoginComponent} from './login/login.component';
+import {HomeuserComponent} from './homeuser/homeuser.component';
+import {TicketComponent} from './ticket/ticket.component';
+import {ListuserComponent} from './listuser/listuser.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: 'home', component:  ListFilmComponent},
+    {path: 'addfilm', component:  FilmComponent},
+    {path: 'updatefilm/:id', component:  UpdatefilmComponent},
+    {path: 'Detailfilm/:id', component:   DetailfilmComponent},
+    {path: 'reserver/:id', component:   ReserverComponent },
+    {path: 'register', component:  RegistrationComponent },
+    {path: 'login', component:  LoginComponent},
+    {path: 'homeuser', component:  HomeuserComponent},
+    {path: 'ticket/:id', component: TicketComponent},
+    {path: 'list', component: ListuserComponent},
+    {path: '**', component:  LoginComponent},
+    ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,44 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilmComponent } from './film/film.component';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
+import { ListFilmComponent } from './list-film/list-film.component';
+import { UpdatefilmComponent } from './updatefilm/updatefilm.component';
+import { DetailfilmComponent } from './detailfilm/detailfilm.component';
+import {ReserverComponent} from './reserver/reserver.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { HomeuserComponent } from './homeuser/homeuser.component';
+import { TicketComponent } from './ticket/ticket.component';
+import { ListuserComponent } from './listuser/listuser.component';
+import { UserComponent } from './user/user.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilmComponent,
+    ListFilmComponent,
+    UpdatefilmComponent,
+    DetailfilmComponent,
+    ReserverComponent,
+    RegistrationComponent,
+    LoginComponent,
+    HomeuserComponent,
+    TicketComponent,
+    ListuserComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
